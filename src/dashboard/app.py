d2373206b5c085b33b11b7f9a0d1f569ce8d8bda
Lucas-Gates <lucascgates@gmail.com>
2026-09-18 19:26:02 -0500

    added docker to simplify app

--- src/dashboard/app.py
+++ src/dashboard/app.py
@@ -2,8 +2,9 @@ import streamlit as st
 import requests
 import pandas as pd
 import plotly.express as px
+import os
 
-API_BASE_URL = "http://127.0.0.1:8000/api"
+API_BASE_URL = os.getenv("API_BASE_URL", "http://127.0.0.1:8000/api")
 
 st.set_page_config(
     page_title="Traffic Safety Intelligence Platform",
